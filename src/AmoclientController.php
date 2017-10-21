c320d6396a59121ef7a1bbee416668d44c7bb45e
Bart Roos <bart@studiokaa.co>
2017-10-21 18:06:49 +0200

    First attempt at redirect

--- src/AmoclientController.php
+++ src/AmoclientController.php
@@ -11,7 +11,13 @@ class AmoclientController extends Controller
 
 	public function redirect()
 	{
-		return redirect('https://login.amo.rocks/oauth/authorize');
+		$client_id = env('AMO_CLIENT_ID');
+		if($client_id == null)
+		{
+			dd('Please set AMO_CLIENT_ID and AMO_CLIENT_SECRET in .env file');
+		}
+
+		return redirect('https://login.amo.rocks/oauth/authorize?client_id=' . $client_id . '&redirect_id=' . url('amoclient/callback') . '&response_type=code');
 	}
 
 	public function callback(Request $request)
