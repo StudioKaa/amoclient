25e78c0035088af22e6a693be5c0b4fb4014204d
Bart Roos <bart@studiokaa.co>
2017-10-21 16:29:51 +0200

    Initial commit

---
+++ src/AmoclientController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace StudioKaa\amoclient;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class AmoclientController extends Controller
+{
+
+	public function redirect()
+	{
+		return redirect('https://login.amo.rocks/oauth/authorize');
+	}
+
+	public function callback(Request $request)
+	{
+		return $request;
+	}
+
+}
---
+++ src/routes.php
@@ -0,0 +1,4 @@
+<?php
+
+Route::get('amoclient/redirect', 'StudioKaa\Amoclient\AmoclientController@redirect');
+Route::get('amoclient/callback', 'StudioKaa\Amoclient\AmoclientController@callback');
