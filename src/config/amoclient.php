bbbb344bbc5c90557e521667e5410fd0909612f9
Sven Luijten <svenluijten1995@gmail.com>
2018-05-24 23:11:24 +0200

    remove defaults of 'null' in 'env()' calls

--- src/config/amoclient.php
+++ src/config/amoclient.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'client_id' => env('AMO_CLIENT_ID', null),
-    'client_secret' => env('AMO_CLIENT_SECRET', null),
+    'client_id' => env('AMO_CLIENT_ID'),
+    'client_secret' => env('AMO_CLIENT_SECRET'),
     'app_for' => env('AMO_APP_FOR', 'teachers'),
     'use_migration' => env('AMO_USE_MIGRATION', 'yes'),
     'api_log' => env('AMO_API_LOG', 'no')
